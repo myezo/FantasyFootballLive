664371de49e1a11475d3b013ce0f721f2f30cb19
myezo <mikeyyz315@gmail.com>
2017-05-02 19:52:41 -0400

    second table

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,13 +3,28 @@ var router = express.Router();
 var mysql = require('mysql');
 var connection = require('../config/connection.js')
 
+
 router.get('/', function(req, res){
 	//res.send('hi')
 	connection.query('SELECT * FROM tophundred;', function(err, data){
 		//res.json(data);
 		res.render('index', {tophundred:data});
 	})
+
+	connection.query('SELECT * FROM runningbacks;', function(err, data){
+		//res.json(data);
+		res.render('index', {runningbacks:data});
+	})
 })
 
+/*
+router.get('/rb', function(req, res){
+	//res.send('hi')
+	connection.query('SELECT * FROM runningbacks;', function(err, data){
+		//res.json(data);
+		res.render('index', {runningbacks:data});
+	})
+})
+*/
 
 module.exports = router;
