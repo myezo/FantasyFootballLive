02ca2607e1cf6efeef5f2bc9c182096dd99ce510
myezo <mikeyyz315@gmail.com>
2017-05-03 21:06:31 -0400

    scrape update

--- scrape/scrapedef.js
+++ scrape/scrapedef.js
@@ -21,7 +21,7 @@ connection.connect(function(err) {
 });
 
 
-request('https://fantasyfootballers.org/te-tight-end-nfl-stats/', function (error, response, body) {
+request('https://fantasyfootballers.org/nfl-team-defense-fantasy-stats/', function (error, response, body) {
 
   // console.log(body);
 
@@ -35,7 +35,7 @@ request('https://fantasyfootballers.org/te-tight-end-nfl-stats/', function (erro
     tableRows.slice(1).each(function(i, element) {
       "use strict";
       let cleanRow = [];
-      let justThese = [1, 2, 3, 4, 5, 6, 7, 8, 11];
+      let justThese = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 16, 17, 18, 19];
       let tds = $(element).children('td');
       
       tds.each(function(tdIndex, td){
@@ -46,7 +46,7 @@ request('https://fantasyfootballers.org/te-tight-end-nfl-stats/', function (erro
         if (justThese.indexOf(i) >= 0) return el;
       });
       //player, team, pos, rec, yards, avg, ydsg, tds
-      connection.query("INSERT INTO tightends (team, fpsgm, ptsallowed, intgm, fumrecgm, sacksgm, totalint, totalfum, totalsacks, safeties, deftds, qbpts, rbpts, wrpts, tepts, kickerpts) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
+      connection.query("INSERT INTO defense (team, fpsgm, ptsallowed, intgm, fumrecgm, sacksgm, totalint, totalfum, totalsacks, safeties, deftds, qbpts, rbpts, wrpts, tepts, kickerpts) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
               filteredRow[0],
               filteredRow[1],
               filteredRow[2],
@@ -55,7 +55,14 @@ request('https://fantasyfootballers.org/te-tight-end-nfl-stats/', function (erro
               filteredRow[5],
               filteredRow[6],
               filteredRow[7],
-              filteredRow[8]
+              filteredRow[8],
+              filteredRow[9],
+              filteredRow[10],
+              filteredRow[11],
+              filteredRow[12],
+              filteredRow[13],
+              filteredRow[14],
+              filteredRow[15]
             ], function(err, res) {
               if (err) return console.log(err);
               else console.log('done')
